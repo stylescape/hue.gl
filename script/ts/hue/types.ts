8537f527a518e7730a9584350fec4d427e64b723
Lars van Vianen <lars@vianen.com>
2024-01-10 01:17:45 +0100

    WIP

--- script/ts/hue/types.ts
+++
@@ -1,8 +0,0 @@
-import Color from 'colorjs.io';
-
-
-export type PackageJson = { [key: string]: any }; // Define a more specific type if possible
-export type ColorScheme =any;
-// export type ColorScheme = Record<string, Record<string, Color>>;
-export type BuildFileContent = { [key: string]: any }; // Adjust according to the actual content structure
-export type OutputPaths = string[]; // Or define a more complex type if needed
