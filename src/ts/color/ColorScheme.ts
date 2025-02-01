fecda604dc2714744c2943932a0bd5eff4ec3c53
Lars van Vianen <lars@vianen.com>
2025-02-01 04:09:28 +0100

    Formatting

--- src/ts/color/ColorScheme.ts
+++ src/ts/color/ColorScheme.ts
@@ -1,26 +1,9 @@
-// script/class/class/DirectoryCleaner.ts
-
-// Copyright 2024 Scape Agency BV
-
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-
-// http://www.apache.org/licenses/LICENSE-2.0
-
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-
 // ============================================================================
 // Import
 // ============================================================================
 
-import { ColorSwatch } from './ColorSwatch.js';
-import { pad } from '../util.js';
+import { pad } from "../util.js";
+import { ColorSwatch } from "./ColorSwatch.js";
 
 
 // ============================================================================
@@ -31,7 +14,7 @@ import { pad } from '../util.js';
  * Configuration for generating a color scheme, including steps and bounds for
  * color values.
  */
- type ColorSchemeConfig = {
+type ColorSchemeConfig = {
     prefix?: string;   // Optional prefix for color names.
     h_step?: number;   // Step increment for hue values.
     p_count?: number;  // Total count of colors to generate.
@@ -118,7 +101,7 @@ export class ColorScheme {
                 if (h == 0) {
                     c_cur = 0;
                 };
-                
+
                 let name = this.config.prefix + pad(h.toString(), 3, "0") + (i + 1).toString();
                 let color = new ColorSwatch(h, c_cur, l_cur, name)
                 this.colorList.push(color);
