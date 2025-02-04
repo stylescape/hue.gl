7601022d86ff2d681e256a0bcc6e49b0996b72da
Lars van Vianen <lars@vianen.com>
2025-02-04 05:59:43 +0100

    Update ColorScheme.ts

--- bin/ts/hue/color/ColorScheme.ts
+++ bin/ts/hue/color/ColorScheme.ts
@@ -1,26 +1,12 @@
-// script/class/class/DirectoryCleaner.ts
 
-// Copyright 2023 Scape Agency BV
-
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-
-// http://www.apache.org/licenses/LICENSE-2.0
-
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
 
 // ============================================================================
 // Import
 // ============================================================================
 
-import ColorSwatch from './ColorSwatch.js';
 import { pad } from '../util.js';
+import ColorSwatch from './ColorSwatch.js';
 
 
 // ============================================================================
@@ -104,7 +90,7 @@ class ColorScheme {
                 if (h == 0) {
                     c_cur = 0;
                 };
-                
+
                 let name = this.config.prefix + pad(h.toString(), 3, "0") + (i + 1).toString();
                 let color = new ColorSwatch(h, c_cur, l_cur, name)
                 this.colorList.push(color);
